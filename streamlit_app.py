867cd7ccfa88e2fc927f53effb005ba1b67f8209
Deanxflash <deanxflash@gmail.com>
2025-02-21 13:15:31 +0800

    Update streamlit_app.py
    Up to date

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.write("Choose the fruit you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Snowflake Session
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # Multiselect input for ingredients
